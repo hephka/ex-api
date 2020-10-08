b5105fd69259f6c786e9647debfb7c2070779fd6
hephka <franckthevenin95@gmail.com>
2020-10-08 13:59:09 +0200

    corr

--- src/models/db.js
+++ src/models/db.js
@@ -3,21 +3,21 @@ import message from './message.js'
 import user from './user.js'
 
 export const sequelize = new Sequelize(
-  'db_api1',
-  'db_user',
-  'strongpassword123',
-  {
-    host: 'localhost',
-    port: 5432,
-    dialect: 'postgres',
-    define: {
-      //prevent sequelize from pluralizing table names
-      freezeTableName: true,
-      // prevent sequelize from adding timestamps column in tables
-      //timestamps: false,
-    },
-    //logging: false,
-  }
+    'db_api1',
+    'db_user',
+    'strongpassword123',
+    {
+        host: 'localhost',
+        port: 5432,
+        dialect: 'postgres',
+        define: {
+            //prevent sequelize from pluralizing table names
+            freezeTableName: true,
+            // prevent sequelize from adding timestamps column in tables
+            //timestamps: false,
+        },
+        //logging: false,
+    }
 )
 
 export const User = user(sequelize)
